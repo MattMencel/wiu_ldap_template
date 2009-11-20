7755627a012ee3401cc0f3f64be08d30692b1107
Matt Mencel <mr-mencel@wiu.edu>
2009-11-20 15:14:45 -0600

    added ldap.yml and some gems to environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,8 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "authlogic"
-
+  config.gem "net-ldap", :lib => false, :version => '>=0.0.5'
+  config.gem "activeldap", :lib => "active_ldap", :version => '1.2.0'
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
